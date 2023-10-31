05d6c2ca9a524a56e06fb61e0b5eefe0865d144b
Ivan Kim <xormrdlsrks2@gmail.com>
2023-10-31 16:04:54 +0900

    [REV] update template

--- myapp/views.py
+++ myapp/views.py
@@ -3,8 +3,22 @@ from myapp.forms import MenuForm
 from .models import Menu
 from django.http import JsonResponse
 
-# Add code for form_view() function below
-
 
 def form_view(request):
-    pass
+    form = MenuForm()
+
+    if request.method == "POST":
+        form = MenuForm(request.POST)
+        print(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+
+            lf = Menu(
+                item_name=cd["item_name"],
+                category=cd["category"],
+                description=cd["description"],
+            )
+
+            lf.save()
+            return JsonResponse({"message": "success"})
+    return render(request, "booking.html", {"form": form})
